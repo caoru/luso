d9c041576dcffef3d649013f64b521297ffbc17b
Ikhyun Kim <frag@frag.kr>
2024-02-20 22:49:09 +0900

    Remove style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,10 +27,7 @@ export function Sidebar() {
           </a>
         </li>
       </ul> */}
-      <div
-        className="d-flex flex-column flex-shrink-0 h-100 p-3 bg-body-tertiary"
-        style={{ width: "280px;" }}
-      >
+      <div className="d-flex flex-column flex-shrink-0 h-100 p-3 bg-body-tertiary">
         {/* <a
           href="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
